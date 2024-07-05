8d2189844c3f61c0f88bcec9c1c894f18d272f6c
Zinubett <zainab.abdelkader@etudiant-isi.utm.tn>
2024-07-05 11:33:27 +0200

    feat: add `splash_screen` and `homePage` ui

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,4 @@
+import 'package:event_app/view/screen/splash_screen.dart';
 import 'package:flutter/material.dart';
 
 void main() {
@@ -10,43 +11,12 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Event App',
+      debugShowCheckedModeBanner: false,
       theme: ThemeData(
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
         useMaterial3: true,
       ),
-      home: const MyHomePage(title: 'homePage'),
-    );
-  }
-}
-
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key, required this.title});
-
-  final String title;
-
-  @override
-  State<MyHomePage> createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-        title: Text(widget.title),
-      ),
-      body: const Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            Text(
-              'the app code',
-            ),
-          ],
-        ),
-      ),
-      // This trailing comma makes auto-formatting nicer for build methods.
+      home: Splash_screen(),
     );
   }
 }
---
+++ lib/view/screen/splash_screen.dart
@@ -0,0 +1,80 @@
+import 'package:flutter/material.dart';
+import 'package:event_app/view/screen/homePage.dart';
+
+class Splash_screen extends StatefulWidget {
+  const Splash_screen({Key? key}) : super(key: key);
+
+  @override
+  _Splash_screenState createState() => _Splash_screenState();
+}
+
+class _Splash_screenState extends State<Splash_screen> {
+  @override
+  void initState() {
+    super.initState();
+    initialization();
+  }
+
+  void initialization() async {
+    await Future.delayed(const Duration(milliseconds: 2000));
+    Navigator.pushReplacement(
+        context, MaterialPageRoute(builder: (context) => HomePage()));
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.transparent,
+      body: _buildBody(),
+    );
+  }
+
+  _buildBody() {
+    return Column(
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: [
+        Expanded(flex: 4, child: Container()),
+        Container(
+          alignment: Alignment.center,
+          padding: EdgeInsets.only(left: 15, right: 15),
+          child: const Text('App Name',
+              style: TextStyle(
+                fontSize: 24,
+                color: Colors.white,
+                fontFamily: 'Aclonica',
+              )),
+        ),
+        SizedBox(
+          height: 10,
+        ),
+        Container(
+          alignment: Alignment.center,
+          padding: EdgeInsets.only(left: 15, right: 15),
+          child: const Text(
+              'Streamline Your Event Planning, Amplify Your Success!',
+              maxLines: 2,
+              textAlign: TextAlign.center,
+              style: TextStyle(
+                fontSize: 14,
+                color: Colors.white,
+                fontFamily: 'DMSans',
+              )),
+        ),
+        Expanded(flex: 4, child: Container()),
+        Container(
+          alignment: Alignment.center,
+          padding: EdgeInsets.only(left: 15, right: 15),
+          child: const Text('Version 0.0.1',
+              maxLines: 2,
+              textAlign: TextAlign.center,
+              style: TextStyle(
+                fontSize: 12,
+                color: Colors.white,
+                fontFamily: 'DMSans',
+              )),
+        ),
+        Expanded(child: Container()),
+      ],
+    );
+  }
+}
